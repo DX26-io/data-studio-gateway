6b561440b0d24584cf92d8afbbf4f16b2165cd62
Flair Viz Dev <flairvizentricdev17@gmail.com>
2021-05-23 11:30:43 +0300

    search api by function

--- src/main/java/com/flair/bi/service/search/SearchService.java
+++ src/main/java/com/flair/bi/service/search/SearchService.java
@@ -2,11 +2,13 @@ package com.flair.bi.service.search;
 
 import com.flair.bi.compiler.search.AggregationStatementResult;
 import com.flair.bi.compiler.search.AggregationStatementsResult;
+import com.flair.bi.compiler.search.ByStatementResult;
 import com.flair.bi.compiler.search.SearchQLCompiler;
 import com.flair.bi.compiler.search.SearchQuery;
 import com.flair.bi.domain.Feature;
 import com.flair.bi.domain.QFeature;
 import com.flair.bi.domain.View;
+import com.flair.bi.domain.enumeration.FeatureType;
 import com.flair.bi.domain.value.QValue;
 import com.flair.bi.domain.value.Value;
 import com.flair.bi.repository.ValueRepository;
@@ -38,14 +40,27 @@ public class SearchService {
     public SearchResult search(Long viewId, String text) {
         com.flair.bi.compiler.search.SearchResult compile = searchQLCompiler.compile(new SearchQuery(text));
 
+        Optional<ByStatementResult> byStatementResult = compile.asByStatementResult();
+        if (byStatementResult.isPresent()) {
+            String featureName;
+            if (byStatementResult.get().getState() == ByStatementResult.State.EXPRESSION) {
+                featureName = null;
+            } else {
+                featureName = byStatementResult.get().lastFeature().orElse(null);
+            }
+            return getAggregationFeatures(viewId, featureName, FeatureType.DIMENSION);
+        }
+
         Optional<AggregationStatementsResult> aggregationStatementsResult = compile.asAggregationStatementsResult();
         if (aggregationStatementsResult.isPresent()) {
             if (aggregationStatementsResult.get().getState() == AggregationStatementsResult.State.EXPRESSION) {
                 Optional<AggregationStatementResult> lastStatement = aggregationStatementsResult.get().lastStatement();
-                if (lastStatement.stream().anyMatch(ls -> ls.getState() == AggregationStatementResult.State.FUNCTION)) {
-                    return getAggregationFunctions(lastStatement.get());
-                } else if (lastStatement.stream().anyMatch(ls -> ls.getState() == AggregationStatementResult.State.FEATURE)) {
-                    return getAggregationFeatures(viewId, lastStatement.get());
+                if (lastStatement.isPresent()) {
+                    if (lastStatement.get().getState() == AggregationStatementResult.State.FUNCTION) {
+                        return getAggregationFunctions(lastStatement.get());
+                    } else if (lastStatement.get().getState() == AggregationStatementResult.State.FEATURE) {
+                        return getAggregationFeatures(viewId, lastStatement.get().getFeature(), FeatureType.MEASURE);
+                    }
                 }
                 return getAggregationFunctions(null);
             }
@@ -56,14 +71,15 @@ public class SearchService {
         return new SearchResult(Collections.emptyList());
     }
 
-    private SearchResult getAggregationFeatures(Long viewId, AggregationStatementResult aggregationStatementResult) {
+    private SearchResult getAggregationFeatures(Long viewId, String featureName, FeatureType featureType) {
         View view = viewService.findOne(viewId);
         Long datasourceId = view.getViewDashboard().getDashboardDatasource().getId();
         List<Feature> features = featureService.getFeatures(QFeature.feature.datasource.id.eq(datasourceId));
 
         List<SearchResult.Item> searchItems = features.stream()
+                .filter(f -> f.getFeatureType() == featureType)
                 .map(v -> new SearchResult.Item(v.getName()))
-                .filter(v -> listOnFilter(v, aggregationStatementResult.getFeature()))
+                .filter(v -> listOnFilter(v, featureName))
                 .collect(Collectors.toList());
 
         return new SearchResult(searchItems);
