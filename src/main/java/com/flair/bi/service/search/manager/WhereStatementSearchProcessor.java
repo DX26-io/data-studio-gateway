2682dc996c054c2055399d43534a72eaf6c1f184
Flair Viz Dev <flairvizentricdev17@gmail.com>
2021-05-29 23:06:12 +0300

    search ql

--- src/main/java/com/flair/bi/service/search/manager/WhereStatementSearchProcessor.java
+++ src/main/java/com/flair/bi/service/search/manager/WhereStatementSearchProcessor.java
@@ -2,11 +2,12 @@ package com.flair.bi.service.search.manager;
 
 import com.flair.bi.compiler.search.WhereConditionResult;
 import com.flair.bi.compiler.search.WhereStatementResult;
+import com.flair.bi.config.jackson.JacksonUtil;
 import com.flair.bi.domain.View;
-import com.flair.bi.domain.enumeration.FeatureType;
 import com.flair.bi.messages.QueryResponse;
 import com.flair.bi.service.GrpcQueryService;
 import com.flair.bi.service.SendGetDataDTO;
+import com.flair.bi.service.search.SearchResult;
 import com.flair.bi.view.ViewService;
 import com.project.bi.query.dto.FieldDTO;
 import com.project.bi.query.dto.QueryDTO;
@@ -14,7 +15,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -49,7 +53,13 @@ public class WhereStatementSearchProcessor implements ISearchQLManagerProcessor 
                                 .type("filters")
                                 .queryDTO(queryDTO)
                                 .build());
-                        return SearchQLManagerProcessorResult.of(searchQLFinder.getAggregationFeatures(input.getViewId(), lastStatement.get().getFeature(), FeatureType.MEASURE));
+                        Map data = JacksonUtil.fromString(queryResponse.getData(), Map.class);
+                        List list = (List) data.get("data");
+                        List<SearchResult.Item> collect = (List<SearchResult.Item>) list.stream().flatMap(l -> ((Map) l).values().stream())
+                                .map(i -> new SearchResult.Item((String) i))
+                                .filter(v -> searchQLFinder.listOnFilter((SearchResult.Item) v, lastStatement.get().getStatement()))
+                                .collect(Collectors.toList());
+                        return SearchQLManagerProcessorResult.of(new SearchResult(collect));
                     }
                 }
                 return SearchQLManagerProcessorResult.of(searchQLFinder.getAggregationFeatures(input.getViewId(), null));
