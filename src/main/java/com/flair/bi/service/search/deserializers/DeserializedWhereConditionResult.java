365c76041e824a13a21b08759e774d6a340b432a
Flair Viz Dev <flairvizentricdev17@gmail.com>
2021-06-08 22:17:20 +0300

    bump flair language

--- src/main/java/com/flair/bi/service/search/deserializers/DeserializedWhereConditionResult.java
+++ src/main/java/com/flair/bi/service/search/deserializers/DeserializedWhereConditionResult.java
@@ -3,10 +3,13 @@ package com.flair.bi.service.search.deserializers;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Data
 public class DeserializedWhereConditionResult {
     private final String feature;
     private final String condition;
     private final String statement;
+    private final List<String> statements;
 }
